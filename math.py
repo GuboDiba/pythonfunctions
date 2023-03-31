23b4b7a548fbe587450c1d8c6ab1b7a1f8b7885c
Gubo Diba <gubodibawario@gmail.com>
2023-03-31 16:05:27 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer = a + b
+    return answer
+
+def substract(x,y):
+    result = x-y
+    return result
+
+def divide(s,t):
+    ans =s/t
+    return ans
+
+def multiply(g,d):
+    res = g*d
+    return result
+
+def remainder(m,n):
+     answers =m%n
+     return answers
